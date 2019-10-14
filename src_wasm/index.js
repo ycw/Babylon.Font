83f2f6ca8b00e71157ef1af7595daf7da402e22d
ycw <ycw7788@gmail.com>
2019-10-15 02:49:42 +0800

    update fontUrl to be in testbed/

--- src_wasm/index.js
+++ src_wasm/index.js
@@ -1,14 +1,35 @@
+//  
+// This file is to prove artifact (at build/untouched.wasm) created by 
+// `npm run asbuild:untouched` works at runtime.
+// 
+// # Note
+// It may pass `npm run asbuild` phase, but then fail at runtime due to 
+// 1. wrong data structure at "load path commands in linear memory" phase 
+// 2. wrong data structure at "map result in linear memory back to js object"
+// 
+// # TODO
+// 1. It seems that assemblyscript `load<T>()` must use little endian 
+//    for multibyte data like f64. So I have to set 3nd param of 
+//    `dataview.setFloat64(,,_)` to be `true` (?)
+// 
+
 const fs = require('fs');
 const { promisify } = require('util');
 const opentype = require('opentype.js');
 const asLoader = require('assemblyscript/lib/loader');
-// const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/build/optimized.wasm"));
 const imports = {
+  //
+  // ---- These are assemblyscript provided, keep ----
+  //
   env: {
     abort(_msg, _file, line, column) {
       console.error("abort called at index.ts:" + line + ":" + column);
     }
   },
+
+  //
+  // ---- These are debug utils when I dev assembly/index.ts ----
+  //
   index: {
     inspectPolygons(str, polygonsPtr) {
       let i = 0;
@@ -25,9 +46,18 @@ const imports = {
     }
   }
 };
+
+
+
+//
+// ---- These are provided by assemblyscript, keep ----
+//
+// const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/build/optimized.wasm"));
 // Object.defineProperty(module, "exports", {
 //   get: () => new WebAssembly.Instance(compiled, imports).exports
 // });
+
+
 
 (async function () {
   const file = await promisify(fs.readFile)(__dirname + '/build/untouched.wasm');
@@ -36,7 +66,7 @@ const imports = {
 
   globalThis.wasm = wasm;
 
-  const fontUrl = '../font/notoserifdisplay-thin.ttf';
+  const fontUrl = '../testbed/font/notoserifdisplay-thin.ttf';
   const ch = 'B';
   const ppc = 0;
   const eps = 0.005;
@@ -44,26 +74,38 @@ const imports = {
   const otFont = await opentype.load(fontUrl);
   const otPath = otFont.getPath(ch, 0, 0, 1);
   const otFontFmt = otFont.outlineFormat;
-  const bytesUsed = loadPathToLinearMemory(wasm, otPath);
 
+  // Test: load data into linear memory
+  const bytesUsed = loadPathToLinearMemory(otPath);
+
+  // Test: wasm exported API "compile" (it uses loaded data)
   const result = wasm.compile(bytesUsed, otFontFmt, ppc, eps);
-  const shapes = map(wasm, result);
+  
+  // Test: map result (in linear memory) to js object 
+  const shapes = map(result);
+
+  // Should print a Set of Shape
   console.log(shapes);
+
+  // If pass, it is safe to run `npm run asbuild:dist` 
+  // which will generate optimized outputs in two places 
+  // 1. /dist
+  // 2. /testbed 
 }());
 
 
 
-function map(wasm, shapesPtr) {
+function map(shapesPtr) {
   const shapes = new Set();
   for (const shapePtr of wasm.__getArray(shapesPtr)) {
-    const shape = { fill: undefined, holes: [] };
+    const shape = { fill: undefined, holes: new Set() };
     shapes.add(shape);
     for (const polygonPtr of wasm.__getArray(shapePtr)) {
       let polygon;
       if (shape.fill === undefined) {
         polygon = shape.fill = [];
       } else {
-        shape.holes.push(polygon = []);
+        shape.holes.add(polygon = []);
       }
       for (const vertexPtr of wasm.__getArray(polygonPtr)) {
         polygon.push(wasm.__getArray(vertexPtr));
@@ -74,7 +116,8 @@ function map(wasm, shapesPtr) {
 }
 
 
-function loadPathToLinearMemory(wasm, otPath) {
+
+function loadPathToLinearMemory(otPath) {
   const heap = wasm.memory.buffer;
   const view = new DataView(heap);
   const SZ = 8;
