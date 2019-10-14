4f190516d516129e09339a71da7f085f3b64b9f5
ycw <ycw7788@gmail.com>
2019-10-15 04:53:29 +0800

    added .module.d.ts

---
+++ dist/compile_wa.module.d.ts
@@ -0,0 +1,18 @@
+declare module "compile_wa" {
+    interface PathCommand {
+        type: string;
+        x?: number;
+        y?: number;
+        x1?: number;
+        y1?: number;
+        x2?: number;
+        y2?: number;
+    }
+    export function init(wasmUrl: string): Promise<(cmds: PathCommand[], fmt: string, ppc?: number, eps?: number) => Set<Shape>>;
+    export type Vertex = [number, number];
+    export type Polygon = Vertex[];
+    export type Shape = {
+        fill: Polygon;
+        holes: Set<Polygon>;
+    };
+}
