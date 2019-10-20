1a308ea345f85773364ae24ad5ca6ef1aaa4c65b
ycw <ycw7788@gmail.com>
2019-10-21 03:56:25 +0800

    update to test Font.js

--- testbed/index.js
+++ testbed/index.js
@@ -4,119 +4,55 @@ main();
 
 async function main() {
 
-    // Make compile fn
-    const wasmUrl = '../src_wasm/build/untouched.wasm';
-    const compile = await compile_wa.init(wasmUrl);
+    const compiler = await Font.InstallCompiler('../dist/compile_wa.wasm');
+    const font = await Font.InstallFont('./font/NotoSerifDisplay-Thin.ttf', compiler);
+    const font2 = await Font.InstallFont('./font/NotoSansCJKtc-Thin.otf', compiler);
 
-    // Load font data via opentypejs
-    const fontUrl = './font/NotoSerifDisplay-Thin.ttf';
-    const font = await opentypeLoadAsync(fontUrl);
-
-    // Create Babylon playground to render compiled shapes
-    createBabylonEnv({ compile, font });
-}
-
-
-
-function createBabylonEnv(state) {
-    const engine = new BABYLON.Engine(elCanvas);
-    const scene = createScene(engine, state);
-    engine.runRenderLoop(() => scene.render());
-    addEventListener('resize', () => engine.resize());
-}
-
-
-
-function createScene(engine, state) {
+    const canvas = document.querySelector('canvas');
+    const engine = new BABYLON.Engine(canvas);
     const scene = new BABYLON.Scene(engine);
+
     scene.createDefaultCamera(true, true, true);
     scene.createDefaultLight(true);
 
-    const text = 'hello\nworld!';
-    const depth = 0.1;
-    const ppc = 5;      // point per curve
-    const eps = 0.001;  // dedup epsilon
-    const tn = createText(text, depth, ppc, eps, state);
-    tn.rotation.x = -Math.PI / 2;
-    tn.scaling.set(0.2, 0.2, 0.2);
+    const option = {
+        depth: 0.1,
+        sideOrientation: BABYLON.Mesh.DOUBLESIDE
+    };
 
-    return scene;
-}
+    let offsetX = 0;
 
+    const char1 = font2.char('我', 1, 10, 0.001);
+    const mesh1 = char1.node(option);
+    mesh1.position.x = offsetX;
+    offsetX += char1.advanceWidth;
 
+    const char2 = font.char('M', 2, 10, 0.001);
+    const mesh2 = char2.node(option);
+    mesh2.position.x = offsetX;
+    mesh2.position.y = 0.1 / 2;
+    offsetX += char2.advanceWidth;
 
-//
-// Minimal multiline text mesh impl.
-//
-
-function createText(text, depth, ppc, eps, { compile, font }) {
-    const tn = new BABYLON.TransformNode('');
-    let z = 0;
-    let x = 0;
-    let xMax = 0;
-    for (const row of text.split('\n')) { // each row
-        for (const ch of row) { // each ch
-
-            //
-            // Compile opentype.PathCommand
-            //
-
-            const otPath = font.getPath(ch, 0, 0, 1);
-            const otFmt = font.outlineFormat;
-            const shapes = compile(otPath.commands, otFmt, ppc, eps);
-
-            //
-            // Consume shapes
-            //
-
-            for (const shape of shapes) { // each shape
-                const mesh = BABYLON.MeshBuilder.ExtrudePolygon('', {
-                    shape: shape.fill.map(vec3), // map to bjs vec3
-                    holes: shape.holes.map(hole => hole.map(vec3)), // ditto
-                    depth,
-                    sideOrientation: BABYLON.Mesh.DOUBLESIDE
-                });
-                mesh.parent = tn;
-                mesh.position.set(x, 0, z);
-            }
-
-            //
-            // Advance next char 
-            //
-
-            x += font.charToGlyph(ch).advanceWidth / font.unitsPerEm;
-            xMax = Math.max(xMax, x);
-        }
-        x = 0;
-        z -= 1.25; // line height
-    }
-    tn.position.x = - xMax / 2;
-    const tn2 = new BABYLON.TransformNode('');
-    tn.parent = tn2;
-    return tn2;
-}
+    const char3 = font.char('e', 2, 5, 0);
+    const node3 = char3.node(option, scene, true);
+    node3.position.x = offsetX;
+    scene.onBeforeRenderObservable.add(() => {
+        node3.rotation.x += 0.01;
+        mesh1.rotation.x += 0.01;
+        mesh2.rotation.x += 0.01;
+    })
 
 
+    const maxA = Math.max(char1.ascender, char2.ascender, char3.ascender);
+    const maxD = Math.min(char1.descender, char2.descender, char3.descender);
 
-//
-// Map bytes from linear memroy to library-dependent struct
-//
-
-function vec3([x, y]) {
-    return new BABYLON.Vector3(x, 0, -y);
-}
-
-
-
-//
-// "Promisify" opentype.load
-//
-
-function opentypeLoadAsync(fontUrl) {
-    return new Promise((resolve, reject) => {
-        opentype.load(fontUrl, (e, font) => {
-            if (e) reject(e);
-            else resolve(font);
-        });
+    const lineA = [new BABYLON.Vector3(0, 0, maxA), new BABYLON.Vector3(10, 0, maxA)];
+    const lineD = [new BABYLON.Vector3(0, 0, maxD), new BABYLON.Vector3(10, 0, maxD)];
+    const baseline = [new BABYLON.Vector3(), new BABYLON.Vector3(10, 0, 0)];
+    BABYLON.MeshBuilder.CreateLineSystem('', {
+        lines: [lineA, baseline, lineD]
     });
+
+    engine.runRenderLoop(() => scene.render());
+    addEventListener('resize', () => engine.resize());
 }
