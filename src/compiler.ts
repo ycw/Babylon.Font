5079718cd1d791c709ba0ded5539880b0d4a23ae
ycw <ycw7788@gmail.com>
2021-09-22 21:56:42 +0800

    init

--- src/compiler.ts
+++ src/compiler.ts
@@ -5,23 +5,25 @@ import * as loader from '@assemblyscript/loader'
 //
 
 interface IPathCommand {
-    type: string;
-    x?: number;
-    y?: number;
-    x1?: number;
-    y1?: number;
-    x2?: number;
-    y2?: number;
+  type: string;
+  x?: number;
+  y?: number;
+  x1?: number;
+  y1?: number;
+  x2?: number;
+  y2?: number;
 }
 
 //
-// Union of assemblyscript loader API & exported "func"s of my ".wasm"
+// API
 //
 
-interface MyAPI {
-    memory: WebAssembly.Memory;
-    compile(bytesUsed: number, fmt: string, ppc: number, eps: number): number;
-}
+type Header = {
+  compile(bytesUsed: number, fmt: string, ppc: number, eps: number): number;
+};
+type Instaniated = loader.ResultObject & {
+  exports: loader.ASUtil & Header
+};
 
 //
 // Shape - Compiled result
@@ -30,180 +32,174 @@ interface MyAPI {
 type Vertex = [number, number];
 type Polygon = Vertex[];
 export type Shape = {
-    fill: Polygon;
-    holes: Polygon[];
+  fill: Polygon;
+  holes: Polygon[];
 };
 
 //
-// Compiler - A wrapper of glue codes
+// Compiler
 //
 
 export class Compiler {
 
-    constructor(private wasm: loader.ASUtil & MyAPI) { }
+  constructor(
+    private wasm: Instaniated
+  ) { }
 
-    //
-    // Build Compiler
-    //
+  static async Build(wasmUrl?: string) {
+    wasmUrl ??= new URL('optimized.wasm', import.meta.url).toString();
 
-    static async Build(wasmUrl: string) {
+    const imports = {
+      // env: {
+      //   abort(_msg, _file, line, column) {
+      //     console.error("abort called at" + line + ":" + column);
+      //   },
+      // },
+    };
 
-        const imports = {
-            env: {
-                abort(_msg, _file, line, column) {
-                    console.error("abort called at" + line + ":" + column);
-                },
-            },
-        };
+    const wasm = await loader.instantiate<Header>(fetch(wasmUrl), imports);
+    return new Compiler(wasm);
+  }
 
-        //
-        // Instantiate
-        // https://caniuse.com/#search=instantiateStreaming
-        //
+  //
+  // Encode OpentypeJS IPathCommand[], put into {buffer}
+  //
 
-        const wasm = await loader.instantiateStreaming<MyAPI>(
-            fetch(wasmUrl),
-            imports
-        );
-        return new Compiler(wasm);
+  encode(cmds: IPathCommand[], buffer: ArrayBuffer) {
+
+    const view = new DataView(buffer);
+
+    let i = 0;
+    let x = 0;
+    let y = 0;
+
+    const M = 'M'.codePointAt(0);
+    const L = 'L'.codePointAt(0);
+    const Q = 'Q'.codePointAt(0);
+    const C = 'C'.codePointAt(0);
+
+    for (const cmd of cmds) {
+      let code = cmd.type.codePointAt(0);
+      view.setUint8(i, code);
+      i++;
+
+      switch (code) {
+        case M:
+        case L:
+          view.setFloat64(i, cmd.x, true);
+          i += 8;
+          view.setFloat64(i, cmd.y, true);
+          i += 8;
+          x = cmd.x;
+          y = cmd.y;
+          break;
+        case Q:
+          view.setFloat64(i, x, true);
+          i += 8;
+          view.setFloat64(i, y, true);
+          i += 8;
+          view.setFloat64(i, cmd.x1, true);
+          i += 8;
+          view.setFloat64(i, cmd.y1, true);
+          i += 8;
+          view.setFloat64(i, cmd.x, true);
+          i += 8;
+          view.setFloat64(i, cmd.y, true);
+          i += 8;
+          x = cmd.x;
+          y = cmd.y;
+          break;
+        case C:
+          view.setFloat64(i, x, true);
+          i += 8;
+          view.setFloat64(i, y, true);
+          i += 8;
+          view.setFloat64(i, cmd.x1, true);
+          i += 8;
+          view.setFloat64(i, cmd.y1, true);
+          i += 8;
+          view.setFloat64(i, cmd.x2, true);
+          i += 8;
+          view.setFloat64(i, cmd.y2, true);
+          i += 8;
+          view.setFloat64(i, cmd.x, true);
+          i += 8;
+          view.setFloat64(i, cmd.y, true);
+          i += 8;
+          x = cmd.x;
+          y = cmd.y;
+          break;
+      }
     }
 
+    return i;
+  }
+
+  //
+  // Compile encoded IPathCommand[] in {buffer}
+  //
+
+  compileEncoded(
+    buffer: ArrayBuffer, bytesUsed: number,
+    fmt: string, ppc: number, eps: number
+  ) {
+
+    ppc = Math.max(0, Math.min(255, Math.round(ppc)));
+    eps = Math.abs(eps);
+
     //
-    // Encode OpentypeJS IPathCommand[], put into {buffer}
+    // Load into memory if needed
     //
 
-    encode(cmds: IPathCommand[], buffer: ArrayBuffer) {
+    if (buffer !== this.wasm.exports.memory.buffer) {
+      const heap = this.wasm.exports.memory.buffer;
+      for (let i = 0, L = buffer.byteLength; i < L; ++i) {
+        heap[i] = buffer[i];
+      }
+    }
 
-        const view = new DataView(buffer);
+    const shapesPtr = this.wasm.exports.compile(bytesUsed, fmt, ppc, eps);
 
-        let i = 0;
-        let x = 0;
-        let y = 0;
+    //
+    // Map to JS Objects
+    //
 
-        const M = 'M'.codePointAt(0);
-        const L = 'L'.codePointAt(0);
-        const Q = 'Q'.codePointAt(0);
-        const C = 'C'.codePointAt(0);
-
-        for (const cmd of cmds) {
-            let code = cmd.type.codePointAt(0);
-            view.setUint8(i, code);
-            i++;
-
-            switch (code) {
-                case M:
-                case L:
-                    view.setFloat64(i, cmd.x, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y, true);
-                    i += 8;
-                    x = cmd.x;
-                    y = cmd.y;
-                    break;
-                case Q:
-                    view.setFloat64(i, x, true);
-                    i += 8;
-                    view.setFloat64(i, y, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.x1, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y1, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.x, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y, true);
-                    i += 8;
-                    x = cmd.x;
-                    y = cmd.y;
-                    break;
-                case C:
-                    view.setFloat64(i, x, true);
-                    i += 8;
-                    view.setFloat64(i, y, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.x1, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y1, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.x2, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y2, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.x, true);
-                    i += 8;
-                    view.setFloat64(i, cmd.y, true);
-                    i += 8;
-                    x = cmd.x;
-                    y = cmd.y;
-                    break;
-            }
+    const F64 = new Float64Array(this.wasm.exports.memory.buffer);
+    const shapesIn = this.wasm.exports.__getUint32Array(shapesPtr);
+    const shapesOut: Shape[] = [];
+    for (let i = 0; i < shapesIn.length; i++) {
+      const shape: Shape = { fill: [], holes: [] };
+      if (shapesIn[i] === 0) break;
+      const polygons = this.wasm.exports.__getUint32Array(shapesIn[i]);
+      for (const polygonPtr of polygons) {
+        if (polygonPtr === 0) break;
+        const vertices = this.wasm.exports.__getUint32Array(polygonPtr);
+        let arr: Polygon;
+        if (shape.fill.length === 0) {
+          arr = shape.fill;
+        } else {
+          shape.holes.push(arr = []);
         }
-
-        return i;
-    }
-
-    //
-    // Compile encoded IPathCommand[] in {buffer}
-    //
-
-    compileEncoded(
-        buffer: ArrayBuffer, bytesUsed: number,
-        fmt: string, ppc: number, eps: number
-    ) {
-
-        ppc = Math.max(0, Math.min(255, Math.round(ppc)));
-        eps = Math.abs(eps);
-
-        //
-        // Load into wasm.memory if needed
-        //
-
-        if (buffer !== this.wasm.memory.buffer) {
-            const heap = this.wasm.memory.buffer;
-            for (let i = 0, L = buffer.byteLength; i < L; ++i) {
-                heap[i] = buffer[i];
-            }
+        for (const vertexPtr of vertices) {
+          if (vertexPtr === 0) break;
+          arr.push([
+            F64[(vertexPtr >>> 3)], // x
+            F64[(vertexPtr >>> 3) + 1]  // y
+          ]);
         }
-
-        const shapesPtr = this.wasm.compile(bytesUsed, fmt, ppc, eps);
-
-        //
-        // Map to JS Objects
-        //
-
-        const F64 = new Float64Array(this.wasm.memory.buffer);
-        const shapesIn = this.wasm.__getUint32Array(shapesPtr);
-        const shapesOut = new Array<Shape>(shapesIn.length);
-        for (let i = 0; i < shapesIn.length; i++) {
-            const shape: Shape = { fill: [], holes: [] };
-            const polygons = this.wasm.__getUint32Array(shapesIn[i]);
-            for (const polygonPtr of polygons) {
-                const vertices = this.wasm.__getUint32Array(polygonPtr);
-                let arr: Polygon;
-                if (shape.fill.length === 0) {
-                    arr = shape.fill;
-                } else {
-                    shape.holes.push(arr = []);
-                }
-                for (const vertexPtr of vertices) {
-                    arr.push([
-                        F64[(vertexPtr >>> 3) + 0], // x
-                        F64[(vertexPtr >>> 3) + 1]  // y
-                    ]);
-                }
-            }
-            shapesOut[i] = shape;
-        }
-        return shapesOut;
+      }
+      shapesOut.push(shape);
     }
+    return shapesOut;
+  }
 
-    //
-    // Compile non-encoded IPathCommand[]
-    //
+  //
+  // Compile non-encoded IPathCommand[]
+  //
 
-    compile(cmds: IPathCommand[], fmt: string, ppc: number, eps: number) {
-        const buffer = this.wasm.memory.buffer;
-        const bytesUsed = this.encode(cmds, this.wasm.memory.buffer);
-        return this.compileEncoded(buffer, bytesUsed, fmt, ppc, eps);
-    }
+  compile(cmds: IPathCommand[], fmt: string, ppc: number, eps: number) {
+    const buffer = this.wasm.exports.memory.buffer;
+    const bytesUsed = this.encode(cmds, this.wasm.exports.memory.buffer);
+    return this.compileEncoded(buffer, bytesUsed, fmt, ppc, eps);
+  }
 }
--- src/font.ts
+++ src/font.ts
@@ -1,6 +1,5 @@
-/// <reference path='../node_modules/babylonjs/babylon.module.d.ts' />
-
-import { Compiler, Shape } from './compiler'
+import * as BABYLON from "babylonjs";
+import { Compiler, Shape } from "./compiler";
 
 
 
@@ -9,13 +8,13 @@ import { Compiler, Shape } from './compiler'
 //
 
 type PolygonMeshOption = {
-    backUVs?: BABYLON.Vector4;
-    depth?: number;
-    faceColors?: BABYLON.Color4[];
-    faceUV?: BABYLON.Vector4[];
-    frontUVs?: BABYLON.Vector4;
-    sideOrientation?: number;
-    updatable?: boolean;
+  backUVs?: BABYLON.Vector4;
+  depth?: number;
+  faceColors?: BABYLON.Color4[];
+  faceUV?: BABYLON.Vector4[];
+  frontUVs?: BABYLON.Vector4;
+  sideOrientation?: number;
+  updatable?: boolean;
 }
 
 
@@ -26,39 +25,39 @@ type PolygonMeshOption = {
 
 export class Font {
 
-    private constructor(
-        public raw: opentype.Font,
-        private compiler: Compiler
-    ) { }
+  private constructor(
+    public raw: opentype.Font,
+    private compiler: Compiler
+  ) { }
 
-    static async Install(
-        fontUrl: string,
-        compiler: Compiler,
-        opentype: any,
-    ) {
-        const raw = await opentype.load(fontUrl);
-        return new Font(raw, compiler);
-    }
+  static async Install(
+    fontUrl: string,
+    compiler: Compiler,
+    opentype: any,
+  ) {
+    const raw = await opentype.load(fontUrl);
+    return new Font(raw, compiler);
+  }
 
-    measure(
-        name: string,
-        size: number
-    ) {
-        return new Metrics(this, name, size);
-    }
+  measure(
+    name: string,
+    size: number
+  ) {
+    return new Metrics(this, name, size);
+  }
 
-    static Compile(
-        font: Font,
-        name: string,
-        size: number,
-        ppc: number,
-        eps: number
-    ) {
-        const cmds = font.raw.getPath(name, 0, 0, size).commands;
-        const fmt = font.raw.outlinesFormat;
-        const shapes = font.compiler.compile(cmds, fmt, ppc, eps * size);
-        return shapes;
-    }
+  static Compile(
+    font: Font,
+    name: string,
+    size: number,
+    ppc: number,
+    eps: number
+  ) {
+    const cmds = font.raw.getPath(name, 0, 0, size).commands;
+    const fmt = font.raw.outlinesFormat;
+    const shapes = font.compiler.compile(cmds, fmt, ppc, eps * size);
+    return shapes;
+  }
 
 }
 
@@ -69,58 +68,58 @@ export class Font {
 //
 
 interface IBabylon {
-    Mesh: typeof BABYLON.Mesh;
-    MeshBuilder: typeof BABYLON.MeshBuilder;
-    Vector3: typeof BABYLON.Vector3
+  Mesh: typeof BABYLON.Mesh;
+  MeshBuilder: typeof BABYLON.MeshBuilder;
+  Vector3: typeof BABYLON.Vector3
 }
 
 export class TextMeshBuilder {
 
-    constructor(
-        private babylon: IBabylon,
-        private earcut: any
-    ) { }
+  constructor(
+    private babylon: IBabylon,
+    private earcut: any
+  ) { }
 
-    private createFromShapes(
-        shapes: Shape[],
-        option: PolygonMeshOption,
-        scene?: BABYLON.Scene,
-    ) {
-        const meshes: BABYLON.Mesh[] = [];
-        const toVec3 = (vert) => new this.babylon.Vector3(vert[0], 0, -vert[1]);
-        for (const { fill, holes } of shapes) {
-            const mesh = this.babylon.MeshBuilder.CreatePolygon('', {
-                ...option,
-                shape: fill.map(toVec3),
-                holes: holes.map(hole => hole.map(toVec3))
-            }, scene, this.earcut);
-            meshes.push(mesh);
-        }
-
-        if (meshes.length > 0) {
-            return this.babylon.Mesh.MergeMeshes(meshes, true, true);
-        } else {
-            return null;
-        }
+  private createFromShapes(
+    shapes: Shape[],
+    option: PolygonMeshOption,
+    scene?: BABYLON.Scene,
+  ) {
+    const meshes: BABYLON.Mesh[] = [];
+    const toVec3 = (vert) => new this.babylon.Vector3(vert[0], 0, -vert[1]);
+    for (const { fill, holes } of shapes) {
+      const mesh = this.babylon.MeshBuilder.CreatePolygon('', {
+        ...option,
+        shape: fill.map(toVec3),
+        holes: holes.map(hole => hole.map(toVec3))
+      }, scene, this.earcut);
+      meshes.push(mesh);
     }
 
-    create({
-        font,
-        text,
-        size = 100,
-        ppc = 2,
-        eps = 0.001,
-        ...option
-    }: {
-        font: Font,
-        text: string,
-        size: number,
-        ppc: number,
-        eps: number,
-    } & PolygonMeshOption, scene?: BABYLON.Scene) {
-        const shapes = Font.Compile(font, text, size, ppc, eps);
-        return this.createFromShapes(shapes, option, scene);
+    if (meshes.length > 0) {
+      return this.babylon.Mesh.MergeMeshes(meshes, true, true);
+    } else {
+      return null;
     }
+  }
+
+  create({
+    font,
+    text,
+    size = 100,
+    ppc = 2,
+    eps = 0.001,
+    ...option
+  }: {
+    font: Font,
+    text: string,
+    size: number,
+    ppc: number,
+    eps: number,
+  } & PolygonMeshOption, scene?: BABYLON.Scene) {
+    const shapes = Font.Compile(font, text, size, ppc, eps);
+    return this.createFromShapes(shapes, option, scene);
+  }
 }
 
 
@@ -131,25 +130,25 @@ export class TextMeshBuilder {
 
 export class Metrics {
 
-    constructor(
-        private font: Font,
-        private name: string,
-        private size: number
-    ) { }
+  constructor(
+    private font: Font,
+    private name: string,
+    private size: number
+  ) { }
 
-    get ascender() {
-        return this.font.raw.ascender
-            / this.font.raw.unitsPerEm
-            * this.size;
-    }
+  get ascender() {
+    return this.font.raw.ascender
+      / this.font.raw.unitsPerEm
+      * this.size;
+  }
 
-    get descender() {
-        return this.font.raw.descender
-            / this.font.raw.unitsPerEm
-            * this.size;
-    }
+  get descender() {
+    return this.font.raw.descender
+      / this.font.raw.unitsPerEm
+      * this.size;
+  }
 
-    get advanceWidth() {
-        return this.font.raw.getAdvanceWidth(this.name, this.size);
-    }
+  get advanceWidth() {
+    return this.font.raw.getAdvanceWidth(this.name, this.size);
+  }
 }
