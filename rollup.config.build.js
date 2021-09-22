91c2d8956d2584f9f9fe8f6a58fbca97c99a1a22
ycw <ycw7788@gmail.com>
2021-09-22 21:53:40 +0800

    init

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,36 +1,29 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import minify from 'rollup-plugin-babel-minify';
-import dts from 'rollup-plugin-dts';
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
 
 export default [{
-    input: './src/babylon.font.ts',
+    input: "./src/index.ts",
     output: {
-        file: './dist/babylon.font.js',
-        format: 'es',
-        sourcemap: true
+        file: "./build/babylon.font.js",
+        format: "es",
+        sourcemap: false
     },
     plugins: [
-        // Resolve source specifier by node style
-        resolve(),
-        // If a source is in commonjs style, transpiles it to ESM
-        commonjs(),
-        // Intergrate with typescript
         typescript({
-            tsconfig: 'tsconfig.json',
             useTsconfigDeclarationDir: true
         }),
-        // Minify
-        minify({
-            comments: false
+        terser({
+            format: {
+                comments: false
+            }
         })
     ]
 }, {
-    input: './dist/dts/babylon.font.d.ts',
+    input: "./typings/index.d.ts",
     output: [{
-        file: './dist/babylon.font.d.ts',
-        format: 'es',
+        file: "./build/babylon.font.d.ts",
+        format: "es",
     }],
     plugins: [
         dts()
